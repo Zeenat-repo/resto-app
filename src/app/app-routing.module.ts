c2cb9151b3fcae6904744af169e8301e05897f7c
zeenat <zeenat.pervin06@gmail.com>
2021-08-27 20:02:45 +0530

    Resto-app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddRestaurentComponent } from './add-restaurent/add-restaurent.component';
+import { ListRestaurentComponent } from './list-restaurent/list-restaurent.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { UpdateRestaurentComponent } from './update-restaurent/update-restaurent.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {component:AddRestaurentComponent,path:'add'},
+  {component:UpdateRestaurentComponent,path:'update'},
+  {component:ListRestaurentComponent,path:'list'},
+  {component:LoginComponent,path:'login'},
+  {component:RegisterComponent,path:'register'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddRestaurentComponent } from './add-restaurent/add-restaurent.component';
+import { UpdateRestaurentComponent } from './update-restaurent/update-restaurent.component';
+import { ListRestaurentComponent } from './list-restaurent/list-restaurent.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import{HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddRestaurentComponent,
+    UpdateRestaurentComponent,
+    ListRestaurentComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
